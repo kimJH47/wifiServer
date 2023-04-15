b7a0e2a54be5e2fcf132092ea1c6525792f9db11
kimJaeHyeon <kmr2644@gmail.com>
2023-04-16 08:12:49 +0900

    feat: 요청 갯수를받아 공공 api 를 받아오는 client 구현

---
+++ src/main/java/com/core/wifiserver/client/PublicApiResponseClient.java
@@ -0,0 +1,39 @@
+package com.core.wifiserver.client;
+
+
+import com.core.wifiserver.client.dto.PublicApiConfig;
+import com.core.wifiserver.client.dto.ResponseEntity;
+import com.google.gson.Gson;
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Request.Builder;
+import okhttp3.Response;
+
+public class PublicApiResponseClient {
+
+    public ResponseEntity getPublicWifiList(int start, int end) {
+        return getResponseEntity(start, end);
+    }
+
+    private ResponseEntity getResponseEntity(int start, int end) {
+        try {
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request build = new Builder().url(createURI(start, end))
+                    .get()
+                    .build();
+            Response execute = okHttpClient.newCall(build).execute();
+            if (execute.isSuccessful()) {
+                Gson gson = new Gson();
+                return gson.fromJson(execute.body().string(), ResponseEntity.class);
+            }
+            throw new IllegalArgumentException("응답실패");
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private String createURI(int start, int end) {
+        return PublicApiConfig.createURI("json", "TbPublicWifiInfo", start, end);
+    }
+}
