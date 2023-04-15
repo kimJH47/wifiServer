90808af2005c65742211ad89da13925bd2a3e763
kimJaeHyeon <kmr2644@gmail.com>
2023-04-16 08:15:36 +0900

    test: api 요청을 받아서 정확한 응답이 오는지 확인하는 테스트 작성

---
+++ src/test/java/com/core/wifiserver/client/PublicApiResponseClientTest.java
@@ -0,0 +1,25 @@
+package com.core.wifiserver.client;
+
+import com.core.wifiserver.client.dto.PublicApiConfig;
+import com.core.wifiserver.client.dto.ResponseEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PublicApiResponseClientTest {
+    @Test
+    @DisplayName("실제 요청테스트) 공공api 에 옮바른 형식의 url 을 get 요청을 보내면 ResponseEntity 에 파싱되어 반환되어야한다.")
+    void get() throws Exception {
+        //refactor -> used mock
+        //given
+        PublicApiResponseClient publicApiResponseClient = new PublicApiResponseClient();
+        //when
+        ResponseEntity responseEntity = publicApiResponseClient.getPublicWifiList(0,
+                PublicApiConfig.getMaxResponseCount());
+        //then
+        String resultCode = responseEntity.getResultCode();
+        Assertions.assertThat(resultCode).isEqualTo("INFO-000");
+        Assertions.assertThat(responseEntity.getWifiInfos()).hasSize(PublicApiConfig.getMaxResponseCount());
+
+    }
+}
