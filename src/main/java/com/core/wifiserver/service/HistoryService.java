97657ffe03899e891546eb0202e857a6f2346645
kimJaeHyeon <kmr2644@gmail.com>
2023-04-17 22:29:38 +0900

    refactor: 결과 response 객체로 매핑

--- src/main/java/com/core/wifiserver/service/HistoryService.java
+++ src/main/java/com/core/wifiserver/service/HistoryService.java
@@ -1,8 +1,12 @@
 package com.core.wifiserver.service;
 
 import com.core.wifiserver.dao.HistoryDao;
+import com.core.wifiserver.dto.HistoryDto;
+import com.core.wifiserver.dto.StatusCode;
 import com.core.wifiserver.dto.request.HistoryRequest;
 import com.core.wifiserver.dto.request.Request;
+import com.core.wifiserver.dto.response.Response;
+import java.util.List;
 
 public class HistoryService {
     private final HistoryDao historyDao;
@@ -11,14 +15,26 @@ public class HistoryService {
         this.historyDao = historyDao;
     }
 
-    public int save(Request<HistoryRequest> historyRequestRequest) {
-        return historyDao.save(historyRequestRequest.getEntity().getLatitude(),
-                historyRequestRequest.getEntity().getLongitude());
+    public Response<?> save(Request<HistoryRequest> historyRequestRequest) {
+        return  Response.builder()
+                .statusCode(StatusCode.SUCCESS)
+                .entity(historyDao.save(historyRequestRequest.getEntity().getLatitude(),
+                        historyRequestRequest.getEntity().getLongitude()))
+                .build();
+
     }
 
-    public int delete(int id) {
-        return historyDao.delete(id);
+    public Response<Integer> delete(int id) {
+        return Response.<Integer>builder().
+                statusCode(StatusCode.SUCCESS).
+                entity(historyDao.delete(id)).
+                build();
     }
 
-
+    public Response<List<HistoryDto>> findAll() {
+        return Response.<List<HistoryDto>>builder().
+                statusCode(StatusCode.SUCCESS).
+                entity(historyDao.findAll()).
+                build();
+    }
 }
