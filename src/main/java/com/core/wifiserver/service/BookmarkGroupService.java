d2b85bb06ac74bd1a165483692e3797f9cff2d8a
kimJaeHyeon <kmr2644@gmail.com>
2023-04-18 19:57:39 +0900

    refactor: 반환 타입 변경

--- src/main/java/com/core/wifiserver/service/BookmarkGroupService.java
+++ src/main/java/com/core/wifiserver/service/BookmarkGroupService.java
@@ -3,6 +3,7 @@ package com.core.wifiserver.service;
 import com.core.wifiserver.dao.BookmarkGroupDao;
 import com.core.wifiserver.dto.BookmarkGroupDto;
 import com.core.wifiserver.dto.StatusCode;
+import com.core.wifiserver.dto.request.Request;
 import com.core.wifiserver.dto.response.Response;
 
 public class BookmarkGroupService {
@@ -12,15 +13,12 @@ public class BookmarkGroupService {
         this.bookmarkGroupDao = new BookmarkGroupDao();
     }
 
-    public Response<Integer> save(BookmarkGroupDto bookmarkGroupDto) {
+    public Response<Integer> save(Request<BookmarkGroupDto> bookmarkGroupRequest) {
         return Response.<Integer>builder()
                 .statusCode(StatusCode.SUCCESS)
-                .entity(bookmarkGroupDao.save(bookmarkGroupDto.getName(), bookmarkGroupDto.getOrder()))
+                .entity(bookmarkGroupDao.save(bookmarkGroupRequest.getEntity().getName(),
+                        bookmarkGroupRequest.getEntity().getOrder()))
                 .build();
 
     }
-
-
-
-
 }
