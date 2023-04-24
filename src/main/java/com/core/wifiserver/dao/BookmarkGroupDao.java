0689073a9c8393b0307eea53384ae778a4a92786
kimJaeHyeon <kmr2644@gmail.com>
2023-04-24 22:22:36 +0900

    refactor: 우선순위 업데이트 및 orders 컬럼명 수정

--- src/main/java/com/core/wifiserver/dao/BookmarkGroupDao.java
+++ src/main/java/com/core/wifiserver/dao/BookmarkGroupDao.java
@@ -17,13 +17,46 @@ public class BookmarkGroupDao {
     }
 
     public int save(String name, int order) {
+        if (isSameOrder(order)) {
+            updateAllOrder(order);
+        }
         String query = QueryBuilderFactory.createInsertQueryBuilder(TABLE_NAME)
                 .addColumn("name").value(name)
-                .addColumn("order").value(order)
+                .addColumn("orders").value(order)
                 .addColumn("create_date").value(LocalDateTime.now().toString())
                 .addColumn("modified_date").value("")
                 .build();
         return jdbcContext.executeSQL(query);
+    }
+
+    public int update(int id, int order, String name) {
+        if (isSameOrder(order)) {
+            updateAllOrder(order);
+        }
+        String query = QueryBuilderFactory.createUpdateQueryBuilder(TABLE_NAME)
+                .addColumn("name").value(name)
+                .addColumn("orders").value(order)
+                .addColumn("modified_date").value(LocalDateTime.now().toString())
+                .where(String.format("id = %d", id))
+                .build();
+        return jdbcContext.executeSQL(query);
+    }
+
+    private boolean isSameOrder(int order) {
+        String query = QueryBuilderFactory.createSelectQueryBuilder(TABLE_NAME)
+                .columns("COUNT(orders)")
+                .where(String.format("orders = %d", order))
+                .build();
+
+        return jdbcContext.select(query, resultSet -> resultSet.getInt(1) >= 1);
+    }
+
+    private void updateAllOrder(int order) {
+        String query = "update BOOKMARK_GROUP"
+                + " set ORDERS = ORDERS + 1"
+                + ",modified_date = " + String.format("\"%s\"",LocalDateTime.now())
+                + " where ORDERS >= " + order;
+        jdbcContext.executeSQL(query);
     }
 
     public int delete(int id) {
@@ -42,22 +75,12 @@ public class BookmarkGroupDao {
                 bookmarkGroupDtos.add(new BookmarkGroupDto(
                         resultSet.getInt("id"),
                         resultSet.getString("name"),
-                        resultSet.getInt("order"),
+                        resultSet.getInt("orders"),
                         resultSet.getString("create_date"),
                         resultSet.getString("modified_date")
                 ));
             }
             return bookmarkGroupDtos;
         });
-    }
-
-    public int update(int id, int order, String name) {
-        String query = QueryBuilderFactory.createUpdateQueryBuilder(TABLE_NAME)
-                .addColumn("order").value(order)
-                .addColumn("name").value(name)
-                .addColumn("modified_date").value(LocalDateTime.now().toString())
-                .where(String.format("id = %d", id))
-                .build();
-        return jdbcContext.executeSQL(query);
     }
 }
