15c963b122e4c8b37753aa31fe904cd054cbcc65
kimJaeHyeon <kmr2644@gmail.com>
2023-04-29 22:32:15 +0900

    refactor: save 파라미터 타입 변경

--- src/main/java/com/core/wifiserver/service/BookmarkListService.java
+++ src/main/java/com/core/wifiserver/service/BookmarkListService.java
@@ -3,6 +3,7 @@ package com.core.wifiserver.service;
 import com.core.wifiserver.dao.BookmarkListDao;
 import com.core.wifiserver.dto.BookmarkListDto;
 import com.core.wifiserver.dto.StatusCode;
+import com.core.wifiserver.dto.request.BookmarkSaveRequest;
 import com.core.wifiserver.dto.request.Request;
 import com.core.wifiserver.dto.response.Response;
 import java.util.List;
@@ -13,12 +14,11 @@ import lombok.RequiredArgsConstructor;
 public class BookmarkListService {
     private final BookmarkListDao bookmarkListDao;
 
-    public Response<Integer> save(Request<BookmarkListDto> request) {
-        BookmarkListDto entity = request.getEntity();
+    public Response<Integer> save(Request<BookmarkSaveRequest> request) {
+        BookmarkSaveRequest entity = request.getEntity();
         return Response.<Integer>builder()
                 .statusCode(StatusCode.SUCCESS)
-                .entity(bookmarkListDao.save(entity.getWifiName(), entity.getId()
-                ))
+                .entity(bookmarkListDao.save(entity.getWifiName(), entity.getBookmarkGroupId()))
                 .build();
     }
 
