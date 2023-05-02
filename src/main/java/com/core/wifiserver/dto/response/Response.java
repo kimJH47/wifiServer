ebc11e62e3a993173ce06848dd4c5519cddbfbca
kimJaeHyeon <kmr2644@gmail.com>
2023-05-02 22:12:16 +0900

    refactor: totalCount 필드 추가

--- src/main/java/com/core/wifiserver/dto/response/Response.java
+++ src/main/java/com/core/wifiserver/dto/response/Response.java
@@ -1,14 +1,17 @@
 package com.core.wifiserver.dto.response;
 
 import com.core.wifiserver.dto.StatusCode;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@AllArgsConstructor
 @Getter
 @Builder
 public class Response<T> {
     private final StatusCode statusCode;
+    private int totalCount;
     private final T entity;
 }
