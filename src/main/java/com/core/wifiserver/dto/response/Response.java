3c6117ea721ba096bb222ea8545a22356d6f25d2
kimJaeHyeon <kmr2644@gmail.com>
2023-05-07 05:50:19 +0900

    fix: @AllArgsConstructor 삭제

--- src/main/java/com/core/wifiserver/dto/response/Response.java
+++ src/main/java/com/core/wifiserver/dto/response/Response.java
@@ -1,12 +1,10 @@
 package com.core.wifiserver.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@AllArgsConstructor
 @Getter
 @Builder
 public class Response<T> {
