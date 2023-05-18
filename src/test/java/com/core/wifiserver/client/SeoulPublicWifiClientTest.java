84547afcb1a468c55f45349f78e796948710133e
kimJaeHyeon <kmr2644@gmail.com>
2023-05-18 09:19:59 +0900

    style: 주석제거

--- src/test/java/com/core/wifiserver/client/SeoulPublicWifiClientTest.java
+++ src/test/java/com/core/wifiserver/client/SeoulPublicWifiClientTest.java
@@ -1,16 +1,18 @@
 package com.core.wifiserver.client;
 
-import com.core.wifiserver.client.config.PublicApiConfig;
-import com.core.wifiserver.client.dto.ResponseEntity;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import com.core.wifiserver.client.config.PublicApiConfig;
+import com.core.wifiserver.client.dto.ResponseEntity;
+
 class SeoulPublicWifiClientTest {
+
+
     @Test
     @DisplayName("실제 요청테스트) 공공api 에 옮바른 형식의 url 을 get 요청을 보내면 ResponseEntity 에 파싱되어 반환되어야한다.")
     void get() throws Exception {
-        //refactor -> used mock
         //given
         SeoulPublicWifiClient seoulPublicWifiClient = new SeoulPublicWifiClient();
         //when
